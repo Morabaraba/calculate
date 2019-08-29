6cce16ef3383e463fb350ffb26c690d3a6f5fb78
d-_-b Planeteer <vcs@ludo.co.za>
2019-08-29 16:59:22 +0000

    js refreshDisplay

--- calculate/static/js/scripts.js
+++ calculate/static/js/scripts.js
@@ -1,7 +1,24 @@
 /* global jQuery _ Backbone */
 (function(root, $, _, Backbone) {
-    $(main)
-    function main() {
-        console.log('main')
+    $(main) // on document ready call main
+
+    var calculator = {
+        display: '0',
+        number1: undefined,
+        number2: undefined, 
+        waitingForNumber2: false,
+        operation: undefined,
+        
+        $display: undefined
     }
+    
+    function refreshDisplay() {
+        calculator.$display.val(calculator.display)           
+    }
+    
+    function main() {
+        calculator.$display = $('.calculator-screen')
+        refreshDisplay()
+    }
+
 })(window, jQuery, _, Backbone)
