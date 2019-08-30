2b392cbb2148127b13510cbfde2a2f909075485d
d-_-b Planeteer <vcs@ludo.co.za>
2019-08-30 17:01:08 +0000

    state 3 transistions

--- calculate/calculator.py
+++ calculate/calculator.py
@@ -6,21 +6,29 @@ from transitions import Machine
 
 input_numbers = '0123456789.'
 input_operations = '+-*/'
+input_equal = '='
 
 states = ['initial', 'transition_from_initial', 'transition', 'transition_from_transition', 'trailing', 'transition_from_trailing', 'equal']
 
 transitions = [
 	# state 1: initial
 	{ 'trigger': 'reset', 'source': 'initial', 'dest': 'initial', 'after': 'after_initial' },
-	{ 'trigger': 'digit', 'source': 'initial', 'dest': 'transition_from_initial', 'after': 'after_digit' },
+	{ 'trigger': 'number', 'source': 'initial', 'dest': 'transition_from_initial', 'after': 'after_number1' },
 	{ 'trigger': 'operation', 'source': 'initial', 'dest': 'transition', 'after': 'after_operation' },
 	{ 'trigger': 'equal', 'source': 'initial', 'dest': 'equal', 'after': 'after_equal' },
 
 	# state 2: transition_from_initial
 	{ 'trigger': 'reset', 'source': 'transition_from_initial', 'dest': 'transition_from_initial', 'conditions': 'is_number1_not_zero', 'after': 'after_initial' },
 	{ 'trigger': 'reset', 'source': 'transition_from_initial', 'dest': 'initial', 'conditions': 'is_number1_not_zero', 'after': 'after_initial' },
-	{ 'trigger': 'digit', 'source': 'transition_from_initial', 'dest': 'transition_from_initial', 'after': 'after_digit' },
+	{ 'trigger': 'number', 'source': 'transition_from_initial', 'dest': 'transition_from_initial', 'after': 'after_number1' },
 	{ 'trigger': 'operation', 'source': 'transition_from_initial', 'dest': 'transition', 'after': 'after_operation' },
+	{ 'trigger': 'equal', 'source': 'transition_from_initial', 'dest': 'equal', 'after': 'after_equal' },
+
+	# state 3: transition
+	{ 'trigger': 'reset', 'source': 'transition', 'dest': 'transition_from_initial', 'after': 'after_reset' },
+	{ 'trigger': 'number', 'source': 'transition', 'dest': 'transition_from_transition', 'after': 'after_number2' },
+	{ 'trigger': 'operation', 'source': 'transition', 'dest': 'transition', 'after': 'after_operation' },
+	{ 'trigger': 'equal', 'source': 'transition', 'dest': 'equal', 'after': 'after_equal' },
 ]
 
 
@@ -32,16 +40,23 @@ class Calculator(object):
 
 
 	# after transitions
-	def after_initial(self):
+	def after_initial(self, event):
 		self.number1 = '0'
 		self.operation1 = '+'
 		self.number2 = '0'
 		self.operation2 = '+'
+		self.number_trailing = '0'
 		self.display = self.number1
 
-	def after_digit(self, event):
+	def after_reset(self, event):
+		self.number1 = '0'
+
+	def after_number1(self, event):
 		self.number1 = '0' + event.number
 		self.display = self.number1
+
+	def after_number2(self, event):
+		self.number2 =  event.number
 
 	def after_operation(self, event):
 		self.operation1 = event.operation
