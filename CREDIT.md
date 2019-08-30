38f86c1ef9bfc0a51c0a3c9030f98b6bf6cb2301
d-_-b Planeteer <vcs@ludo.co.za>
2019-08-30 16:07:56 +0000

    state 1, initial state setup

---
+++ CREDIT.md
@@ -0,0 +1,5 @@
+### Resources and References 
+
+- [Designing A Calculator with FSM Logic](https://medium.com/@rvunabandi/making-a-calculator-in-javascript-64193ea6a492)
+- [Finite State Machine and the Cheap Calculator](https://www.clear.rice.edu/comp212/06-spring/labs/13/)
+
--- calculate/calculator.py
+++ calculate/calculator.py
@@ -1,8 +1,38 @@
 from transitions import Machine
 
 class Calculator(object):
-	pass
+
+	def __init__(self):
+		self.after_initial()
+
+	def after_initial(self):
+		self.number1 = 0
+		self.operation1 = '+'
+		self.number2 = 0
+		self.operation2 = '+'
+		self.display = self.number1
+
+	def after_digit(self, event):
+		print('after_digit')
+		pass
+
+	def after_operation(self, event):
+		print('after_digit')
+		pass
+
+	def after_equal(self, event):
+		print('after_equal')
+		pass
+
+states = ['initial', 'transition_from_initial', 'transition', 'transition_from_transition', 'trailing', 'transition_from_trailing', 'equal']
+
+transitions = [
+	{ 'trigger': 'reset', 'source': 'initial', 'dest': 'initial', 'after': 'after_initial' },
+	{ 'trigger': 'digit', 'source': 'initial', 'dest': 'transition from initial', 'after': 'after_digit' },
+	{ 'trigger': 'operation', 'source': 'initial', 'dest': 'transition', 'after': 'after_operation' },
+	{ 'trigger': 'equal', 'source': 'initial', 'dest': 'equal', 'after': 'after_equal' },
+]
 
 calculator = Calculator()
 
-machine = Machine(model=calculator, states=['initial', 'transition from initial', 'transition', 'transition from transition', 'trailing', 'transition from trailing', 'equal'], initial='initial')
+machine = Machine(model=calculator, states=states, transitions=transitions, initial='initial', send_event=True)
