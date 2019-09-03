2946815b6237043feed7c672ef1fdee5edfc23b0
d-_-b Planeteer <vcs@ludo.co.za>
2019-09-03 12:43:24 +0000

    added click support

--- calculate/__main__.py
+++ calculate/__main__.py
@@ -1,8 +1,19 @@
+import click
+from transitions.extensions import GraphMachine
+
 from .calculator import Calculator
 
-def main():
+@click.command()
+@click.option('--debug', is_flag=True, help='Enter the python debug repl. (default)')
+@click.option('--graph', is_flag=True, help='Draw the calculator state machine.')
+@click.option('--output', default='state_diagram.png', help='Used with --graph to specify filename for drawing. (state_diagram.png)')
+def main(debug, graph, output):
 	calculator = Calculator()
-	import pdb; pdb.set_trace()
-
+	if graph:
+		machine = GraphMachine(model=calculator,  states=Calculator.states, transitions=Calculator.transitions, use_pygraphviz=False)
+		machine.get_graph().draw(output, prog='dot')
+	else: # default debug
+		import pdb; pdb.set_trace()
+	
 if __name__ == '__main__':
 	main()
